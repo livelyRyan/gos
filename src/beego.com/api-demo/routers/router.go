2b710eb813c49a9d60fe311ee14562bad87aa17e
livelyRyan <1135038815@qq.com>
2020-05-12 21:13:28 +0800

    添加过滤器功能

--- src/beego.com/api-demo/routers/router.go
+++ src/beego.com/api-demo/routers/router.go
@@ -7,7 +7,11 @@ package routers
 import (
 	"beego.com/api-demo/controllers"
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/context"
+	"net/http"
 )
+
+const authKey = "uid"
 
 func init() {
 	ns := beego.NewNamespace("/v1",
@@ -35,12 +39,11 @@ func init() {
 	beego.AddNamespace(ns)
 
 	// 添加过滤器
-	/*var FilterUser = func(ctx *context.Context) {
-		_, ok := ctx.Input.Session("uid").(int)
-		if !ok && ctx.Request.RequestURI != "/login" {
+	var FilterUser = func(ctx *context.Context) {
+		if ctx.Input.Session(authKey) == nil && ctx.Request.RequestURI != "/login" {
 			ctx.Output.Status = http.StatusUnauthorized
 			ctx.Output.Body([]byte("请进行登录"))
 		}
 	}
-	beego.InsertFilter("/v1/demo/index", beego.BeforeRouter, FilterUser, true)*/
+	beego.InsertFilter("/v1/demo/index", beego.BeforeRouter, FilterUser, true)
 }
